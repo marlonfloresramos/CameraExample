061b5d5eb0b5d146014213bda20975360b8ecae4
marlonfr <marlon.floresramos07@gmail.com>
2023-04-13 11:50:43 -0500

    Initial Commit

---
+++ CameraExample/CameraExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraExampleApp.swift
+//  CameraExample
+//
+//  Created by Marlon Gabriel Flores Ramos on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
